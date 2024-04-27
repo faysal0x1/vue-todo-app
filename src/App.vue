734c22517d42c8fe01872de2ba4c63766e8c5d43
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2024-04-27 12:46:31 +0600

    Card Ui Design

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,46 @@
 
 <template>
 
+
+
+  <div class="container">
+    <div class="row my-5">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="cart-title text-center">Task List</h4>
+                    <div class="row mb-4">
+                        <form action="" class="">
+                            <div class="col-md-12">
+                                <label for="" class="form-label">Add new Task</label>
+                                <input type="text" class="form-control">
+                            </div>
+
+                           <div class="mt-3">
+                            <input type="button" class="btn btn-success"
+                            value="Add Task">
+                           </div>
+                    
+                        </form>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <h4 class="cart-title text-center">Task List</h4>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center">Hello
+
+                        <a href="" class="btn"><i class="fa-solid fa-xmark"></i></a>
+
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Custom Css
+ import './assets/sass/style.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
 
 app.mount('#app')
+
+
+// Import Js from bootstrap
+
+import * as bootstrap from 'bootstrap'
